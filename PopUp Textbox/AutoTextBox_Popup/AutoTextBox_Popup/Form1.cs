bf318733053e4aaaba4bb254a0b03b82fe92166d
Yash Mandaliya <112624754+yashmandaliya@users.noreply.github.com>
2023-02-14 09:22:52 +0530

    Add files via upload

---
+++ PopUp Textbox/AutoTextBox_Popup/AutoTextBox_Popup/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+namespace AutoTextBox_Popup
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection cn = new SqlConnection(@"Data Source=.\SQLEXPRESS;AttachDbFilename=|DataDirectory|\Database1.mdf;Integrated Security=True;User Instance=True");
+            cn.Open();
+            string sql = "insert into Table1 values('"+textBox1.Text+"','"+textBox2.Text+"','"+textBox3.Text+"')";
+            SqlCommand cmd = new SqlCommand(sql,cn);
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Your Data Is Inserted");
+            cn.Close();
+        }
+
+    }
+}
